d30fdebe952b699f360be85f0b4253dbc3d30678
andrei26916 <andrey.stosh@yandex.ru>
2023-03-06 01:26:51 +0800

    (+) base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use core\Response;
+
 class Controller
 {
+    /**
+     * @return void
+     */
     public function index()
     {
-        echo 'hello';
+        echo "<h1 style='text-align: center'>Hello World </h1>";
+
+        echo "<center><a href='https://github.com/andrei26916/macro'> GitHub </a></center>";
     }
 
 }
